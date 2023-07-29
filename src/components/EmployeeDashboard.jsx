34fe97ae6c584d7721dd09eecb94b2a04d64e617
Aswathykumpalathu <aswathykumpalathu@gmail.com>
2023-07-29 14:54:06 +0300

    search userdashboard aswathy

--- src/components/EmployeeDashboard.jsx
+++ src/components/EmployeeDashboard.jsx
@@ -1,66 +1,87 @@
+import React, { useEffect, useState } from 'react';
+import { Button, Table, TableBody, TableCell, TextField, Typography, TableHead, TableRow } from '@mui/material';
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
 
 const EmployeeDashboard = () => {
-    const [data,setData]=useState([]);
-    const[update,setUpdate]=useState(false);
-    const[singleValue,setSingleValue]=useState([]);
+  const [search, setSearch] = useState('');
+  const [data, setData] = useState([]);
   const [userrole, setUserRole] = useState('');
-  const fetchDataFromApi= ()=>{
-    axios.get("http://localhost:5000/api/curriculumlist/" ).then(
-    (response)=>{
-        console.log(response.data)
-        setData(response.data)
-    }
-    )
-}
+
+  const fetchDataFromApi = () => {
+    axios.get("http://localhost:5000/api/curriculumlist/")
+      .then((response) => {
+        console.log(response.data);
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  };
+
   useEffect(() => {
     const storedUserRole = sessionStorage.getItem('userrole');
     setUserRole(storedUserRole);
     fetchDataFromApi();
-  }, []); 
-  
+  }, []);
 
+  const searchCurriculum = () => {
+    console.log('button clicked');
+    // Convert the search state to an object with the field "query"
+    const searchQuery = { query: search };
+    axios.post("http://localhost:5000/api/curriculum/search",  { query: search })
+      .then((response) => {
+        console.log(response.data);
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  };
 
   return (
     <div>
-    <div className="container mt-5 pt-5">
-<div>
-<table class="table">
-<thead>
-<tr>
-<th scope="col">ID</th>
-<th scope="col">Requirement</th>
-<th scope="col">Area</th>
-<th scope="col">Institution</th>
-<th scope="col">Category</th>
-<th scope="col">Hours</th>
+      <div className="container mt-5 pt-5">
+      <Typography>Search</Typography>
+        <TextField
+          name='search'
+          variant='outlined'
+          color="success"
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+          label='Search'
+          fullWidth
+          margin='normal'
+        />
+        <Button onClick={searchCurriculum} variant='contained' color='primary' fullWidth>Search</Button>
+        <div>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>ID</TableCell>
+                <TableCell>Requirement</TableCell>
+                <TableCell>Area</TableCell>
+                <TableCell>Institution</TableCell>
+                <TableCell>Category</TableCell>
+                <TableCell>Hours</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data.map((value, index) => (
+                <TableRow key={index}>
+                  <TableCell>{index}</TableCell>
+                  <TableCell>{value.requirementname}</TableCell>
+                  <TableCell>{value.area}</TableCell>
+                  <TableCell>{value.institution}</TableCell>
+                  <TableCell>{value.category}</TableCell>
+                  <TableCell>{value.hours}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-</tr>
-</thead>
-<tbody>
-
-{data.map((value,index)=>{
-        return<tr key={index}>
-            <td>{index}</td>
-      <td>{value.requirementname}</td>
-      <td>{value.area}</td>
-      <td>{value.institution}</td>
-      <td>{value.category}</td>
-      <td>{value.hours}</td>
-      
-      </tr>
-      })}
-</tbody>
-</table>
-</div>
-</div>
-</div>
-
-    
-  )
-}
-
-
-export default EmployeeDashboard
-
+export default EmployeeDashboard;
